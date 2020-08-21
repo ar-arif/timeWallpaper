2e3ff2dd16db100e0d0f1786422681a562ccf8a8
AR <ar.arif7497.1@gmail.com>
2020-08-21 11:02:14 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+
+let date = new Date()
+let curTime = date.getHours()
+let value = ""
+if (curTime>=1 && curTime<12){
+  value = "Mornig"
+}
+else if(curTime>=12 && curTime<19){
+  value = "Afternoon"
+}
+else{
+  value = "Night"
+}
+ReactDOM.render(
+  <>
+    <img src={`https://source.unsplash.com/1000x1000/?nature,${value}`}></img>
+    <h1>Hellow Sir, Good {value}</h1>
+  </>,
+  document.getElementById("root")
+);
